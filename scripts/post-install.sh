41586baccd200eab15bff4502451bbe82186b490
Danyang CHANG <danyang.chang@sap.com>
2024-10-09 05:26:35 +0000

    bind to 127.0.00.1 and disable detection of systemd

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -3,14 +3,16 @@ set -e
 
 PID1=$(ps --no-headers -o comm 1)
 
-if [ "$PID1" != systemd ]; then
-	echo "Only systemd is supported but detected pid 1: $PID1"
-	exit 1
-fi
+# if [ "$PID1" != systemd ]; then
+# 	echo "Only systemd is supported but detected pid 1: $PID1"
+# 	exit 1
+# fi
 
-echo "Detected systemd as init system, proceeding"
-
-useradd -r nvidia_gpu_exporter -s /bin/false || true
+# echo "Detected systemd as init system, proceeding"
+echo "Add user and group"
+groupadd nvidia_gpu_exporter
+useradd -r -g nvidia_gpu_exporter nvidia_gpu_exporter -s /bin/false || true
+chown nvidia_gpu_exporter:nvidia_gpu_exporter /usr/bin/nvidia_gpu_exporter
 
 systemctl daemon-reload
 
--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -3,7 +3,7 @@ set -e
 
 PID1=$(ps --no-headers -o comm 1)
 
-if [ "$PID1" != systemd ]; then
-	echo "Only systemd is supported but detected pid 1: $PID1"
-	exit 1
-fi
+# if [ "$PID1" != systemd ]; then
+# 	echo "Only systemd is supported but detected pid 1: $PID1"
+# 	exit 1
+# fi
